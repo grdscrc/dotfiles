187abd0729f7d62edbdad26f67bff2a836527f43
Igor Descayrac <igor.descayrac@nrco.fr>
2024-10-25 15:39:09 +0200

    Add pyenv

--- nrco.bash
+++ nrco.bash
@@ -179,3 +179,21 @@ source "$HOME/.cargo/env"
 source /home/unix/.config/broot/launcher/bash/br
 
 source ~/dotfiles/nrco_funcs.bash
+
+# WARNING: seems you still have not added 'pyenv' to the load path.
+
+# Load pyenv automatically by appending
+# the following to
+# ~/.bash_profile if it exists, otherwise ~/.profile (for login shells)
+# and ~/.bashrc (for interactive shells) :
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# Restart your shell for the changes to take effect.
+
+# Load pyenv-virtualenv automatically by adding
+# the following to ~/.bashrc:
+
+eval "$(pyenv virtualenv-init -)"
